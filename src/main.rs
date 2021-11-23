4daa38baebef0fb437e3bcfc200cec3672383aa9
Tnevins <travisnevins@outlook.com>
2021-11-22 19:27:45 -0500

    adding read me and simple hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+//main entry of app
+//Reminder let varibles are immutable 
 fn main() {
+    //str didn't have to be used but for clarity why not?
+    let msg = "Rust projects 1";
+    println!("{}",msg);
     println!("Rust project! Bevy!");
 }
